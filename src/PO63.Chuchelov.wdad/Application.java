79f54883e8ad0570bf8195a948e5fa833c27c1ed
EugeneChuchelov <38725251+EugeneChuchelov@users.noreply.github.com>
2018-10-31 15:38:43 +0400

    Changed JAXB to DOM+xPath

--- src/PO63.Chuchelov.wdad/Application.java
+++
@@ -1,7 +0,0 @@
-package PO63.Chuchelov.wdad;
-
-public class Application {
-    public static void main(String[] args){
-        System.out.println("I'm Evgeniy Chuchelov, and I'm not a monkey");
-    }
-}
